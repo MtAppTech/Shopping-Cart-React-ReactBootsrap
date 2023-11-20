e6b8378d103ee355eba76db4e585115a3061d140
murat <yazilimgenel@gmail.com>
2023-11-20 14:45:24 +0100

    mac

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import AppNavbar from "./components/AppNavbar";
+import About from "./pages/About";
+import Main from "./pages/Main";
+import NewProduct from "./pages/NewProduct";
+import ProductList from "./pages/ProductList";
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Router>
+      
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/newProduct" element={<NewProduct />} />
+        <Route path="/productList" element={<ProductList />} />
+      </Routes>
+    </Router>
   );
 }
 
