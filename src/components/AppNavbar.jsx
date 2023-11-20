e23ff75b89d32ce0ea4f9358134b6f16de163332
murat <yazilimgenel@gmail.com>
2023-11-20 22:23:35 +0100

    mac

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Container, Nav } from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link, NavLink } from "react-router-dom";
 
 const AppNavbar = () => {
   return (
-    <div>AppNavbar</div>
-  )
-}
+    <Navbar
+      expand="lg"
+      className=" "
+      style={{ background: "#758280", paddingLeft: "15px" }}
+    >
+      <Navbar.Brand className="text-white">
+        Shopping Cart
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <NavLink className="nav-link" to="/">
+            Home
+          </NavLink>
+          <NavLink className="nav-link" to="/newProduct">
+            New Product
+          </NavLink>
+          <NavLink className="nav-link" to="/productList">
+            Product List
+          </NavLink>
+          <NavLink className="nav-link" to="/about">
+            About
+          </NavLink>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
-export default AppNavbar
+export default AppNavbar;
