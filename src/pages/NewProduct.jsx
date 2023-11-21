378f271428b506aae67d3bd390928cb6201b1a69
murat <yazilimgenel@gmail.com>
2023-11-21 09:57:15 +0100

    mac

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from "react";
+import ProductForm from "../components/ProductForm";
 
 const NewProduct = () => {
   return (
-    <div>NewProduct</div>
-  )
-}
+    <div className="container">
+      <article
+        id="add-product"
+        className="mb-4 mt-4 col col-lg-6 mx-auto border rounded-2 bg-opacity-50 bg-light"
+      >
+        <h1 className="text-center"> New Product</h1>
 
-export default NewProduct
+        <ProductForm />
+      </article>
+    </div>
+  );
+};
+
+export default NewProduct;
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
+import CardTotal from "../components/CardTotal";
+import axios from "axios";
 
 const ProductList = () => {
-  return (
-    <div>ProductList</div>
-  )
-}
 
-export default ProductList
+  const [products, setProducts] =useState([])
+  return (
+    <div className="container mt-3">
+      <div className="d-sm-block d-md-flex">
+        <>
+          <article id="product-panel" className="col-md-6">
+            {[].map((product) => (
+              <ProductCard key={product.id} />
+            ))}
+          </article>
+          <article className="col-md-4 m-3">
+            <CardTotal />
+          </article>
+        </>
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
